c5d880db86920d2e4cbadc0ca7909a7851012a23
crunchplant <maggiemarkley@msn.com>
2019-10-27 14:14:59 -0400

    nuking errything

--- src/Components/Picts.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export const Picts: React.FC = () => {
-    return (
-        <img />
-    )
-}
--- src/Components/Title.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export const Title: React.FC = () => {
-    return (
-        <h1>Maggie's Portfolio</h1>
-    )
-}
